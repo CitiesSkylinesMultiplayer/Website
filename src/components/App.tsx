ad24b5faf2a2aeffa09414d724edc25d5e12124a
Dominic Maas <dominic.maas@live.com>
2020-06-05 19:06:30 +1200

    Fix up logo description and content

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from '../resources/logo.svg';
 import './App.scss';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark" expand="lg" className="shadow-sm">
+        <Container>
+          <Navbar.Brand>Cities: Skylines Multiplayer</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="https://steamcommunity.com/sharedfiles/filedetails/?id=1558438291">
+                Steam Workshop
+              </Nav.Link>
+              <Nav.Link href="https://www.patreon.com/CSM_MultiplayerMod">
+                Patreon
+              </Nav.Link>
+              <Nav.Link href="https://discord.gg/RjACPhd">
+                Discord Server
+              </Nav.Link>
+              <Nav.Link href="https://github.com/CitiesSkylinesMultiplayer/CSM/">
+                GitHub
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Jumbotron fluid>
+        <Container>
+          <h1>Under Development</h1>
+          <p>
+            This website is still under development. See the links above for
+            more information.
+          </p>
+        </Container>
+      </Jumbotron>
+
+      <footer className="Footer">
+        <Container>
+          <p>
+            <small>
+              This website and mod are in no way affiliated with Cities:
+              Skylines or Paradox Interactive / Colossal Order
+            </small>
+          </p>
+        </Container>
+      </footer>
     </div>
   );
 }
